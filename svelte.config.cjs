d7c5e5767944c3c2ee31b1e365924f759caff09e
Jeremy Wong <jeremy.wong@clearlabs.com>
2021-03-31 22:56:51 -0700

    Modify svelte config for netlify

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,5 +1,7 @@
 const sveltePreprocess = require('svelte-preprocess');
-const node = require('@sveltejs/adapter-node');
+// const node = require('@sveltejs/adapter-node');
+const netlify = require('@sveltejs/adapter-netlify');
+// const static = require('@sveltejs/adapter-static');
 const pkg = require('./package.json');
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,7 +13,7 @@ module.exports = {
 		// By default, `npm run build` will create a standard Node app.
 		// You can create optimized builds for different platforms by
 		// specifying a different adapter
-		adapter: node(),
+		adapter: netlify(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
